b91b6c0e15278e250742dc168bb3e5b703631654
matin <matineh.azad96@gmail.com>
2023-05-14 20:25:56 +0330

    initial project

---
+++ JS/nav.js
@@ -0,0 +1,22 @@
+function showMenu() {
+    let list = document.getElementById("navList");
+    if (list.style.display=== "block"){
+        list.style.display= "none"
+    } else list.style.display= "block"
+}
+$(function () {
+    let list = document.getElementById("header");
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            list.classList.add("sticky");
+        } else {
+            list.classList.remove("sticky")
+        }
+        if ($(this).scrollTop() > 500) {
+            list.classList.add("inView");
+        } else {
+            list.classList.remove("inView")
+        }
+    })
+});
